3921cee901e737f9cb90aa82d78920fad6c33fb0
Zavian <zavian.mail@gmail.com>
2014-02-17 19:21:02 +0100

    Inizio del Progetto
    Inizio progetto per L'ITT "G. Chilesotti". Progetto creato da Emanuele
    Sbabo (developer) e Francesco Lorenzini (designer)

---
+++ CalcProject/mainForm.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace CalcProject {
+    partial class mainForm {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent() {
+            this.rExpressions = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.nuovoSistemaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aiutoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rExpressions
+            // 
+            this.rExpressions.BackColor = System.Drawing.Color.White;
+            this.rExpressions.Location = new System.Drawing.Point(64, 37);
+            this.rExpressions.Name = "rExpressions";
+            this.rExpressions.Size = new System.Drawing.Size(422, 201);
+            this.rExpressions.TabIndex = 0;
+            this.rExpressions.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(79, 243);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Inserisci una espressione per riga";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 120F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(-27, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 182);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "{";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(104, 273);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(352, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Analizza";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nuovoSistemaToolStripMenuItem,
+            this.aiutoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(545, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // nuovoSistemaToolStripMenuItem
+            // 
+            this.nuovoSistemaToolStripMenuItem.Name = "nuovoSistemaToolStripMenuItem";
+            this.nuovoSistemaToolStripMenuItem.Size = new System.Drawing.Size(99, 20);
+            this.nuovoSistemaToolStripMenuItem.Text = "Nuovo Sistema";
+            // 
+            // aiutoToolStripMenuItem
+            // 
+            this.aiutoToolStripMenuItem.Name = "aiutoToolStripMenuItem";
+            this.aiutoToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.aiutoToolStripMenuItem.Text = "Aiuto";
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(545, 308);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rExpressions);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "mainForm";
+            this.Text = "Simple Simplex";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rExpressions;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem nuovoSistemaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aiutoToolStripMenuItem;
+    }
+}
+
---
+++ CalcProject/mainForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CalcProject {
+    public partial class mainForm : Form {
+        public mainForm() {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            string[] s = new string[] { "3x1-3x2-4x3<=360" };
+            cTable t = new cTable(s);
+
+            for (int i = 0; i < t.Numbers.Count; i++) {
+                string tmp = "";
+                for (int j = 0; j < t.Numbers[i].Length; j++) {
+                    tmp += t.Numbers[i][j] + " ";
+                }
+                rExpressions.Text += tmp;
+            }
+            rExpressions.Text += "\n";
+
+            for (int i = 0; i < t.Functions.Length; i++) {
+                rExpressions.Text += t.Functions[i];
+            }
+
+            
+        }
+    }
+}
